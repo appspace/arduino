ea24936905d6ec796a3f815f18024462bc3d9cbe
Eugene Sukharev <eugenes.mobile@gmail.com>
2014-09-27 19:39:44 -0400

    Recent changes

--- RF24Transmitter/RF24Transmitter.ino
+++ RF24Transmitter/RF24Transmitter.ino
@@ -6,8 +6,10 @@
 // Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 9 & 10 
 RF24 radio(9,10);
 //test
-const uint64_t pipe = 0xE8E8F0F0E1LL;
-char data[] = {"b"};
+const uint64_t pipeAtoB = 0xE8E8F0F0E1LL;
+const uint64_t pipeBtoA = 0xE8E8F0F0E2LL;
+
+char data[] = {"Helo"};
 char buf[5];
 char *ptr = &data[0];
 boolean EndOfLine = false;
@@ -24,24 +26,18 @@ void setup() {
   radio.begin();                          // Start up the radio
   radio.setAutoAck(1);                    // Ensure autoACK is enabled
   radio.setRetries(15,15);                // Max delay between retries & number of retries
-  radio.openWritingPipe(pipe);
-  radio.openReadingPipe(1,pipe);
-
-  radio.printDetails();                   // Dump the configuration of the rf unit for debugging
-
+  radio.openWritingPipe(pipeAtoB);
+  radio.openReadingPipe(1,pipeBtoA);
+  //radio.printDetails();                   // Dump the configuration of the rf unit for debugging
   printf("*** WORKING IN TRANSMIT ROLE\n\r");
 }
 
 void loop(void) {
   sendHeader();
+  while (!waitForHeaderResponse()) {
+    sendHeader();
+  }
   
- // printf("Now sending \n\r");
-
- // int len = sizeof(data)-1;
- // printf("%c", *ptr);
- // *ptr++;
- // printf("%c", *ptr);
-//delay(1000);
   strcpy(buf,"\0\0\0\0\0");
   for (int i = 0; i<5 ; i++){
     if (*ptr == '\0') {
@@ -57,25 +53,11 @@ void loop(void) {
    ptr = &data[0];
    EndOfLine = false;
  }
- printf("\n\rBuf done! CRC=%d\n\r", );
   
-  
-
   if (!radio.write( &buf, sizeof(buf) )){
     printf("failed.\n\r");
   }
-  /*  
-  unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-  boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-  while ( ! radio.available() ){                             // While nothing is received
-    if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-      timeout = true;
-      break;
-    }      
-  }
-  */
-    // Try again 500ms later
+     // Try again 500ms later
   delay(500);
   
   
@@ -92,14 +74,43 @@ int calcCRC(char arr[],int len) {
 }
 
 void sendHeader() {
-  radio.openWritingPipe(pipe);
   radio.stopListening();  // First, stop listening so we can talk.
 
   header[0]=0x01;
-  header[1]=(char)sizeof(data);
-  int dataCRC = calcCRC(data, sizeof(data));
-  header[2] = dataCRC;
-  printf("Sending len=%d and waiting for ack", header[1], headerCRC);
-  radio.write( &header, sizeof(header) )
+  uint8_t dataSize = sizeof(data);
 
+  header[1]=dataSize;
+  
+  //int dataCRC = calcCRC(data, sizeof(data));
+  //header[2] = dataCRC;
+  //header[3] = 'O';
+  //header[4] = 'K';
+  printf("Sending len=%d and waiting for ack\n\r", header[1]);
+  radio.write( &header, sizeof(header) );
+}
+
+boolean waitForHeaderResponse() {
+  radio.startListening();                                    // Now, continue listening
+    
+  unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+  boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+  while ( ! radio.available() ){                             // While nothing is received
+    if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+      timeout = true;
+      break;
+    }
+  }
+        
+  if ( timeout ) {                                             // Describe the results
+     printf("Header response timed out.\n\r");
+  } else {
+    char response;                                 // Grab the response, compare, and send to debugging spew
+    radio.read( &response, sizeof(char) );
+    if (response == 0x06) {
+      printf("Header succesfully received!\n\r");
+      return true;
+    }
+  }
+  return false;
 }
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -6,7 +6,9 @@
 // Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 9 & 10 
 RF24 radio(48,49);
 
-const uint64_t pipe = 0xE8E8F0F0E1LL;
+const uint64_t pipeAtoB = 0xE8E8F0F0E1LL;
+const uint64_t pipeBtoA = 0xE8E8F0F0E2LL;
+
 
 // Set up roles to simplify testing 
 boolean role;                                    // The main role variable, holds the current role identifier
@@ -15,29 +17,27 @@ boolean role_ping_out = 1, role_pong_back = 0;   // The two different roles.
 void setup() {
   Serial.begin(115200);
   printf_begin();
-  printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
+  printf("\n\rRF24-receiver\n\r");
 
   // Setup and configure rf radio
   radio.begin();                          // Start up the radio
   radio.setAutoAck(1);                    // Ensure autoACK is enabled
   radio.setRetries(15,15);                // Max delay between retries & number of retries
-  //radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(1,pipe);
-
-  radio.startListening();                 // Start listening
-  radio.printDetails();                   // Dump the configuration of the rf unit for debugging
+  radio.openWritingPipe(pipeBtoA);
+  radio.openReadingPipe(1,pipeAtoB);
 }
 
 void loop(void) {
-  //radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-    if( radio.available()){
-      char data[5];
-      radio.read( &data[0], sizeof(data)+1 );             // Get the payload
-      printf("Received %s\n\r", data);      
-      //radio.stopListening();                                        // First, stop listening so we can talk   
-      //radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      //radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      //printf("Sent response %lu \n\r", got_time);  
-   }
+  radio.startListening();                                       // Now, resume listening so we catch the next packets.     
+  if ( radio.available()) {
+    char data[5];
+    radio.read( &data[0], sizeof(data)+1 );             // Get the payload
+    printf("Received %d%d\n\r", data[3], data[4]);
+    if (data[0]==0x01 && data[3]=='O' && data[4]=='K') {
+      printf("It looks like we received header");
+      radio.stopListening();                                        // First, stop listening so we can talk      
+      char ack = 0x06;
+      radio.write( &ack, sizeof(char) );              // Send the final one back.      
+    }
+  }
 }
