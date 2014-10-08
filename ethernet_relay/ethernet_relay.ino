99fcbf5f9f55114113f7516a47b2da4c9bcbae0a
Eugene Sukharev <eugenes.mobile@gmail.com>
2014-10-07 20:36:08 -0400

    Can drive relays via ethernet!

--- ethernet_relay/ethernet_relay.ino
+++ ethernet_relay/ethernet_relay.ino
@@ -1,29 +1,72 @@
-const int relayPin_1 =  31;
-const int relayPin_2 =  33;
-const int relayPin_3 =  35;
-const int relayPin_4 =  37;
-const int relayPin_5 =  39;
+#include <SPI.h>
+#include <Ethernet.h>
 
-int relayNumActive = 0;
 int RELAY_ON = LOW;
 int RELAY_OFF = HIGH;
+int relayPin[] = {31, 33, 35, 37, 39}; 
+boolean relayState[] = {RELAY_OFF, RELAY_OFF, RELAY_OFF, RELAY_OFF, RELAY_OFF};
+const int pinsConnected = 5;
+byte mac[] = { 0xEE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,1, 177);
+IPAddress gateway(192,168,1, 1);
+IPAddress subnet(255, 255, 0, 0);
+
+EthernetServer server(23);
+boolean alreadyConnected = false; // whether or not the client was connected previously
 
 void setup() {
-  pinMode(relayPin_1, OUTPUT);  digitalWrite(relayPin_1, RELAY_OFF);
-  pinMode(relayPin_2, OUTPUT);  digitalWrite(relayPin_2, RELAY_OFF);
-  pinMode(relayPin_3, OUTPUT);  digitalWrite(relayPin_3, RELAY_OFF);
-  pinMode(relayPin_4, OUTPUT);  digitalWrite(relayPin_4, RELAY_OFF);
-  pinMode(relayPin_5, OUTPUT);  digitalWrite(relayPin_5, RELAY_OFF);
-}
+  for (int i=0; i<pinsConnected; i++) {
+    pinMode(relayPin[i], OUTPUT);
+    digitalWrite(relayPin[i], RELAY_OFF);
+  }
 
-void loop()
-{
-    // set the LED with the ledState of the variable:
-    //digitalWrite(relayPin_1, ledState);
-    //digitalWrite(relayPin_2, ledState);
-    //digitalWrite(relayPin_3, ledState);
-    //digitalWrite(relayPin_4, ledState);
-    //digitalWrite(relayPin_5, ledState);
+  Ethernet.begin(mac, ip, gateway, subnet);
+  server.begin();
   
+  Serial.begin(115200);
+  
+  Serial.print("Relay server address:");
+  Serial.println(Ethernet.localIP());
 }
 
+void loop() {
+  // wait for a new client:
+  EthernetClient client = server.available();
+  // when the client sends the first byte, say hello:
+  if (client) {
+    if (!alreadyConnected) {
+      // clead out the input buffer:
+      client.flush();    
+      Serial.println("We have a new client");
+      client.println("Hello, client!"); 
+      alreadyConnected = true;
+    } 
+
+    if (client.available() > 0) {
+      // read the bytes incoming from the client:
+      char thisChar = client.read();
+      // echo the bytes back to the client:
+      server.write(thisChar);
+      // echo the bytes to the server as well:
+      Serial.write(thisChar);
+      int relayInput = thisChar - 49;
+      if (relayInput>=0 && relayInput<pinsConnected) {
+          boolean state = relayState[relayInput];
+          state = !state;
+          Serial.print("Setting pin ");
+          Serial.print(relayInput, DEC);
+          Serial.print(" to ");
+          Serial.print(state, BIN);
+          relayState[relayInput] = state;
+      }
+    }
+    
+    activateRelays();
+  }
+}
+
+void activateRelays() {
+  for (int i=0; i<pinsConnected; i++) {
+    digitalWrite(relayPin[i], relayState[i]);
+  }
+}
